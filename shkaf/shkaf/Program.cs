79e76fa5baeba11a674348fbae4aacacb4547860
P1kerezka <95563874+P1kerezka@users.noreply.github.com>
2022-06-24 17:02:16 +0300

    Add files via upload

---
+++ shkaf/shkaf/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Zadacha
+{
+    static void Main()
+    {
+
+        int[] a = new int[100];
+        for (int i = 0; i < 100; i++)
+            a[i] = 0;
+
+        for (int b = 1; b <= 100; b++)
+        {
+            for (int c = b; c < 100; c += b)
+            {
+                if (a[c] == 0)
+                {
+                    a[c] = 1;
+                }
+                else
+                {
+                    a[c] = 0;
+                }
+            }
+        }
+        for (int d = 0; d < 100; d++)
+        {
+            if (a[d] == 1)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
